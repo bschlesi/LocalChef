7d7992b8cb8272b3339523685dc1856c1146889b
bschlesi <bschlesinger2006@gmail.com>
2026-08-27 10:55:07 -0400

    documentation + fixing batch exe file

--- run.bat
+++ run.bat
@@ -2,7 +2,7 @@
 echo Starting LocalChef...
 where python >nul 2>nul
 if %errorlevel% equ 0 (
-    python app.py
+    "C:\Users\bschl\AppData\Local\Programs\Python\Python313\python.exe" app.py
 ) else (
     "C:\Users\bschl\AppData\Local\Programs\Python\Python313\python.exe" app.py
 )
